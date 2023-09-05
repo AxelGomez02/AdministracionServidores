07a254f4a447fb00184a90c6218b03018208268b
AxelGomez02 <113264761+AxelGomez02@users.noreply.github.com>
2023-09-04 21:39:04 -0600

    Add files via upload
    Aqui estan los 2 scrips uno recupera los archivos borrados y uno elimina los archivos existentes

---
+++ scrips/Eliminador.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#############################################################
+
+Nombre: AXEL NAIM GOMEZ CARRETA
+CARRERA: Redes y Servicios de Computo 
+Universidad Veracruzana
+
+############################################################
+echo Escribe el nombre del archivo a borrar
+read Nombre
+mv $Nombre /home/axelgomez/.trash
+ 
+
+
---
+++ scrips/Recuperar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo ingresa el nombre del archivo/carpeta a recuperar
+read Nombre
+mv /home/axelgomez/.trash/$Nombre .
